c1d1536c8c1fa6a9cc69e8e4a49d92a21d1b198d
Victor van Acht <victor@octaal.nl>
2024-07-09 19:57:19 +0200

    improved code structure. Added scoring for players.

--- BomberJack/Form1.Designer.cs
+++ BomberJack/Form1.Designer.cs
@@ -33,6 +33,14 @@
             this.labelNumberPlayers = new System.Windows.Forms.Label();
             this.numericUpDownPlayers = new System.Windows.Forms.NumericUpDown();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.labelPlayer1Score = new System.Windows.Forms.Label();
+            this.labelPlayer2Score = new System.Windows.Forms.Label();
+            this.labelPlayer3Score = new System.Windows.Forms.Label();
+            this.labelPlayer4Score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPlayers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -98,11 +106,103 @@
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // labelPlayer1Score
+            // 
+            this.labelPlayer1Score.AutoSize = true;
+            this.labelPlayer1Score.Location = new System.Drawing.Point(554, 133);
+            this.labelPlayer1Score.Name = "labelPlayer1Score";
+            this.labelPlayer1Score.Size = new System.Drawing.Size(51, 15);
+            this.labelPlayer1Score.TabIndex = 6;
+            this.labelPlayer1Score.Text = "Player 1:";
+            this.labelPlayer1Score.Visible = false;
+            // 
+            // labelPlayer2Score
+            // 
+            this.labelPlayer2Score.AutoSize = true;
+            this.labelPlayer2Score.Location = new System.Drawing.Point(554, 158);
+            this.labelPlayer2Score.Name = "labelPlayer2Score";
+            this.labelPlayer2Score.Size = new System.Drawing.Size(51, 15);
+            this.labelPlayer2Score.TabIndex = 7;
+            this.labelPlayer2Score.Text = "Player 2:";
+            this.labelPlayer2Score.Visible = false;
+            // 
+            // labelPlayer3Score
+            // 
+            this.labelPlayer3Score.AutoSize = true;
+            this.labelPlayer3Score.Location = new System.Drawing.Point(554, 183);
+            this.labelPlayer3Score.Name = "labelPlayer3Score";
+            this.labelPlayer3Score.Size = new System.Drawing.Size(51, 15);
+            this.labelPlayer3Score.TabIndex = 8;
+            this.labelPlayer3Score.Text = "Player 3:";
+            this.labelPlayer3Score.Visible = false;
+            // 
+            // labelPlayer4Score
+            // 
+            this.labelPlayer4Score.AutoSize = true;
+            this.labelPlayer4Score.Location = new System.Drawing.Point(554, 209);
+            this.labelPlayer4Score.Name = "labelPlayer4Score";
+            this.labelPlayer4Score.Size = new System.Drawing.Size(51, 15);
+            this.labelPlayer4Score.TabIndex = 9;
+            this.labelPlayer4Score.Text = "Player 4:";
+            this.labelPlayer4Score.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(611, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = " ";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(611, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = " ";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(611, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = " ";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(611, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 21);
+            this.label4.TabIndex = 13;
+            this.label4.Text = " ";
+            this.label4.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 431);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPlayer4Score);
+            this.Controls.Add(this.labelPlayer3Score);
+            this.Controls.Add(this.labelPlayer2Score);
+            this.Controls.Add(this.labelPlayer1Score);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.numericUpDownPlayers);
             this.Controls.Add(this.labelNumberPlayers);
@@ -123,5 +223,13 @@
         private Label labelNumberPlayers;
         private NumericUpDown numericUpDownPlayers;
         private Button buttonStart;
+        private Label labelPlayer1Score;
+        private Label labelPlayer2Score;
+        private Label labelPlayer3Score;
+        private Label labelPlayer4Score;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- BomberJack/Form1.cs
+++ BomberJack/Form1.cs
@@ -4,6 +4,8 @@ using System.Media;
 using System.Security.Cryptography.Xml;
 using System.Windows.Forms;
 
+
+
 namespace BomberJack
 {
     public partial class Form1 : Form
@@ -12,9 +14,25 @@ namespace BomberJack
         private Color[] playerColor = { Color.Gray, Color.Red, Color.Blue, Color.Purple, Color.Green }; // item 0 will be ignored. Players count from 1 to 4
         private Board board;
 
-        private int player=0;
+        private int currentPlayer = 0;
+        private Player[] player;
         private int maxPlayer;
 
+        public class Player
+        {
+            public int score;
+            public bool hasPlayed;
+            public bool isKilled;
+            public Color color;
+
+            public Player(Color color) 
+            { 
+                this.score = 0;
+                this.hasPlayed = false;
+                this.isKilled = false;
+                this.color = color;
+            }
+        }
 
         public class Board
         {
@@ -51,14 +69,14 @@ namespace BomberJack
 
             private Font fnt = new Font("Arial", 10);
             private GroupBox field;
-            private Color[] playerColor;
+            private Player[] player;
 
-            public Board(int countX, int countY, GroupBox field, Color[] playerColor)
+            public Board(int countX, int countY, GroupBox field, Player[] player)
             {
                 this.countX = countX;
                 this.countY = countY;
                 this.field = field;
-                this.playerColor = playerColor;
+                this.player = player;
 
                 this.board = new GameCell[countX, countY];
                 for (int y = 0; y < this.countY; y++)
@@ -71,23 +89,23 @@ namespace BomberJack
                 for (int x = 1; x < this.countX - 1; x++)
                 {
                     this.board[x, 0].maxCapacity = 3;
-                    this.board[x, 0].explosionDirections = new int[,] { { -1, 0 },            { 1, 0 }, { 0, 1 } };
+                    this.board[x, 0].explosionDirections = new int[,] { { -1, 0 }, { 1, 0 }, { 0, 1 } };
                     this.board[x, this.countY - 1].maxCapacity = 3;
-                    this.board[x, this.countY - 1].explosionDirections = new int[,] { { -1, 0 }, { 0, -1 }, { 1, 0 }           };
+                    this.board[x, this.countY - 1].explosionDirections = new int[,] { { -1, 0 }, { 0, -1 }, { 1, 0 } };
                 }
                 for (int y = 1; y < this.countY - 1; y++)
                 {
                     this.board[0, y].maxCapacity = 3;
-                    this.board[0, y].explosionDirections = new int[,] {            { 0, -1 }, { 1, 0 }, { 0, 1 } };
+                    this.board[0, y].explosionDirections = new int[,] { { 0, -1 }, { 1, 0 }, { 0, 1 } };
                     this.board[this.countX - 1, y].maxCapacity = 3;
                     this.board[this.countX - 1, y].explosionDirections = new int[,] { { -1, 0 }, { 0, -1 }, { 0, 1 } };
                 }
                 this.board[0, 0].maxCapacity = 2;
                 this.board[0, 0].explosionDirections = new int[,] { { 0, 1 }, { 1, 0 } };
                 this.board[this.countX - 1, 0].maxCapacity = 2;
-                this.board[this.countX - 1, 0].explosionDirections = new int[,] { { -1, 0}, { 0, 1 } };
+                this.board[this.countX - 1, 0].explosionDirections = new int[,] { { -1, 0 }, { 0, 1 } };
                 this.board[0, this.countY - 1].maxCapacity = 2;
-                this.board[0, this.countY - 1].explosionDirections = new int[,] { { 0 , -1 }, { 1, 0 } };
+                this.board[0, this.countY - 1].explosionDirections = new int[,] { { 0, -1 }, { 1, 0 } };
                 this.board[this.countX - 1, this.countY - 1].maxCapacity = 2;
                 this.board[this.countX - 1, this.countY - 1].explosionDirections = new int[,] { { -1, 0 }, { 0, -1 } };
                 this.field.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
@@ -141,7 +159,7 @@ namespace BomberJack
                             SolidBrush brush;
                             if (board[x, y].count <= board[x, y].maxCapacity)
                             {
-                                brush = new SolidBrush(this.playerColor[board[x, y].owner]);
+                                brush = new SolidBrush(this.player[board[x, y].owner].color);
                             }
                             else
                             {
@@ -160,7 +178,7 @@ namespace BomberJack
                 }
             }
 
-            public bool Click(int clickX, int clickY, int player)
+            public bool Click(int clickX, int clickY, int currentPlayer)
             {
                 // Check boarders
                 if ((clickX < this.windowLeft) || (clickX > this.windowRight) || (clickY < this.windowTop) || (clickY > this.windowBottom)) return false;
@@ -168,14 +186,16 @@ namespace BomberJack
                 int x = (clickX - this.windowLeft) / this.cellSizeX;
                 int y = (clickY - this.windowTop) / this.cellSizeY;
 
-                if ((this.board[x, y].owner != 0) && (this.board[x, y].owner != player)) return false;
+                if ((this.board[x, y].owner != 0) && (this.board[x, y].owner != currentPlayer)) return false;
 
-                this.board[x, y].owner = player;
+                this.board[x, y].owner = currentPlayer;
                 this.board[x, y].count++;
 
-                this.CheckBoard(player);
+                this.CheckBoard(currentPlayer);
 
-                // Redraw
+                this.CountScores();
+
+                //redraw
                 this.field.Invalidate();
                 return true;
             }
@@ -210,43 +230,87 @@ namespace BomberJack
                     }
                 }
             }
+
+            private void CountScores()
+            {
+                // reset all score counters to zero
+                for (int i=0;i<this.player.Length-1;i++)
+                {
+                    this.player[i].score = 0;
+                }
+
+                // count the scores
+                for (int x = 0; x < this.countX; x++)
+                {
+                    for (int y = 0; y < this.countY; y++)
+                    {
+                        if (this.board[x,y].owner != 0)
+                        {
+                            this.player[this.board[x, y].owner].score++;
+                        }
+                    }
+                }
+            }
         }
 
-        public Form1(int countX, int countY)
+    public Form1(int countX, int countY)
         {
             InitializeComponent();
             this.Field.MouseClick += this.ClickField;
             this.countX = countX;
             this.countY = countY;
 
-            this.board = new Board(this.countX, this.countY, this.Field, this.playerColor);
+            this.player = new Player[5];
+            for (int i = 0; i <= 4; i++) { this.player[i] = new Player(this.playerColor[i]); }
+            this.board = new Board(this.countX, this.countY, this.Field, this.player);
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            this.currentPlayer = 1;
+            this.maxPlayer = (int)this.numericUpDownPlayers.Value;
+
             this.numericUpDownPlayers.Enabled = false;
             this.buttonPlayerIndicator.Visible = true;
 
-            this.player = 1;
-            this.maxPlayer = (int)this.numericUpDownPlayers.Value;
+            this.label1.Visible = true;
+            this.labelPlayer1Score.Visible= true;
+            this.label2.Visible = true;
+            this.labelPlayer2Score.Visible = true;
+            if (maxPlayer >= 3)
+            {
+                this.label3.Visible = true;
+                this.labelPlayer3Score.Visible = true;
+            }
+            if (maxPlayer >= 4)
+            {
+                this.label4.Visible = true; ;
+                this.labelPlayer4Score.Visible = true;
+            }
         }
 
         public void DrawPlayerIndicator(object sender, PaintEventArgs e)
         {
-            this.buttonPlayerIndicator.BackColor = this.playerColor[this.player];
-            this.buttonPlayerIndicator.Text = "Player " + this.player.ToString();
+            this.buttonPlayerIndicator.BackColor = this.playerColor[this.currentPlayer];
+            this.buttonPlayerIndicator.Text = "Player " + this.currentPlayer.ToString();
         }
 
         private void ClickField(object sender, MouseEventArgs e)
         {
-            if (player != 0) // cannot play before the game has started
+            if (currentPlayer != 0) // cannot play before the game has started
             {
                 if (e.Button == MouseButtons.Left)
                 {
-                    if (board.Click(e.X, e.Y, this.player))
-                    { // when return true, go to the next player
-                        this.player++;
-                        if (this.player > this.maxPlayer) this.player = 1;
+                    if (board.Click(e.X, e.Y, this.currentPlayer))
+                    {
+                        this.label1.Text = this.player[1].score.ToString();
+                        this.label2.Text = this.player[2].score.ToString();
+                        this.label3.Text = this.player[3].score.ToString();
+                        this.label4.Text = this.player[4].score.ToString();
+
+                        // when return true, go to the next player
+                        this.currentPlayer++;
+                        if (this.currentPlayer > this.maxPlayer) this.currentPlayer = 1;
                         this.buttonPlayerIndicator.Invalidate();
                     }
                 }
